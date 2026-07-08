a592de3ad03e896e1f9b8970750bcac5a848d441
Bramdon Santiago <bramdsantiago@gmail.com>
2026-07-08 10:25:58 -0600

    Add component sidebar

---
+++ frontend/src/app/features/ai-assistant/components/context-panel/context-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-context-panel',
+  imports: [],
+  templateUrl: './context-panel.component.html',
+  styleUrl: './context-panel.component.scss'
+})
+export class ContextPanelComponent {
+
+}
--- frontend/src/app/features/ai-assistant/pages/ai-assistant-page/ai-assistant-page.component.ts
+++ frontend/src/app/features/ai-assistant/pages/ai-assistant-page/ai-assistant-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-ai-assistant-page',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './ai-assistant-page.component.html',
   styleUrl: './ai-assistant-page.component.scss'
 })
